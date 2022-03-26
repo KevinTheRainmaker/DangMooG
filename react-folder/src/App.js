0812ff8de887dd24ddc91365a3f892f6fe9353f4
Kangbeen Ko <sweetrainforyou@gmail.com>
2022-03-26 15:35:48 +0900

    feat: make upload and product page component & implement to App.js

--- react-folder/src/App.js
+++ react-folder/src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 import MainPageComponent from "./main/index.js";
+import UploadPageComponent from "./upload/index.js";
+import ProductPageComponent from "./product/index.js";
+
+import { Switch, Route } from "react-router-dom";
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPageComponent />
+        </Route>
+
+        <Route exact={true} path={"/upload"}>
+          <UploadPageComponent />
+        </Route>
+
+        <Route exact={true} path={"/product"}>
+          <ProductPageComponent />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ react-folder/src/upload/index.js
@@ -0,0 +1,5 @@
+function UploadPage() {
+  return <h1>업로드 페이지</h1>;
+}
+
+export default UploadPage;
